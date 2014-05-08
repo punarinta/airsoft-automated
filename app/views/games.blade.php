bcdde026d67ae780774062226367ff41d056448c
punarinta <cheaterx@yandex.ru>
2014-05-08 17:31:54 +0200

    geopicker

--- app/views/games.blade.php
+++ app/views/games.blade.php
@@ -1,27 +1,41 @@
 @extends('layout')
 
 @section('header')
-
+{{ HTML::style('/css/games.css') }}
 @stop
 
 @section('content')
 <div class="padded_content">
-    <table>
-        <tr>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Region</th>
-            <th>Can book</th>
-        </tr>
-        @foreach ($games as $game)
-        <tr>
-            <td>{{ date('Y.m.d', strtotime($game->starts_at)); }}</td>
-            <td>{{ $game->name; }}</td>
-            <td>{{ $game->region_name; }}</td>
-            <td>{{ $game->bookable ? '+' : '–'; }}</td>
-        </tr>
-        @endforeach
-    </table>
+    <div id="calendar">
+        <div id="calendar-bar">
+            @include('partial/region-picker', ['placement' => 'horizontal', 'style' => 'float:left', 'prefix' => ''])
+            <div style="float:right">
+                <a href="{{ URL::route('game-edit') }}">Create your own!</a>
+            </div>
+        </div>
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Region</th>
+                <th>Booking</th>
+            </tr>
+            @foreach ($games as $game)
+            <tr>
+                <td>{{ date('Y.m.d', strtotime($game->starts_at)); }}</td>
+                <td>{{ $game->name; }}</td>
+                <td>{{ $game->region_name; }}</td>
+                <td>
+                    @if ($game->bookable)
+                    <a href="{{ URL::route('game-book', $game->id) }}">book</a>
+                    @else
+                    no tickets
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 
 
---
+++ app/views/partial/region-picker.blade.php
@@ -0,0 +1,8 @@
+<div style="{{$style}}">
+    <select id="{{$prefix}}country">
+        <option value="0">Any</option>
+    </select>
+    <select id="{{$prefix}}region">
+        <option value="0">Any</option>
+    </select>
+</div>
