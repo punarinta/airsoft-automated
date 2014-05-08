947649761ad0cec0768c152d7ddcbe0bd6ec0d99
punarinta <cheaterx@yandex.ru>
2014-05-08 19:36:51 +0200

    DB updater

--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -7,7 +7,7 @@ mysqladmin -u root -f drop test
 mysqladmin -u root create test
 
 # Import database
-mysql -uroot -D test < ~/src/airsoft-automated/app/database/dumps/`ls -l ~/src/airsoft-automated/app/database/dumps | awk '/coursio/ { f=$NF };END{ print f }'`
+mysql -uroot -D test < ~/src/airsoft-automated/app/database/dumps/`ls -l ~/src/airsoft-automated/app/database/dumps | awk '{ f=$NF };END{ print f }'`
 
 # Flush redis
 # redis-cli -p 16379 flushall
