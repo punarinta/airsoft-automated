66225b8a953ccf4792d91798f55ab459335fed31
punarinta <cheaterx@yandex.ru>
2014-05-09 11:33:21 +0200

    login and remind dialogues

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -1,24 +1,32 @@
 @extends('layout')
 
 @section('content')
-<h1>Login</h1>
+<fieldset class="dialog-box-1" style="width:280px">
+    <legend>Sign-in</legend>
 
-{{ Form::open(array('url' => 'user/login', 'method' => 'post')) }}
+    {{ Form::open(array('url' => 'user/login', 'method' => 'post')) }}
+    <table>
+        <tr>
+            <td>{{ Form::label('username', 'Username:') }}</td>
+            <td>&nbsp;</td>
+            <td><input class="my-input" name="username" type="text" id="username" value="{{ Input::old('username') }}"></td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('password', 'Password:') }}</td>
+            <td>&nbsp;</td>
+            <td><input class="my-input" name="password" type="password" value="" id="password"></td>
+        </tr>
+        <tr>
+            <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <input class="my-btn" type="submit" value="Login">
+                <a href="{{ URL::route('user-restore-password') }}" class="my-btn" >Forgot password</a>
+            </td>
+        </tr>
+    </table>
+    {{ Form::close() }}
+</fieldset>
 
-<!-- username field -->
-<p>
-    {{ Form::label('username', 'Username') }}<br/>
-    {{ Form::text('username', Input::old('username')) }}
-</p>
-
-<!-- password field -->
-<p>
-    {{ Form::label('password', 'Password') }}<br/>
-    {{ Form::password('password') }}
-</p>
-
-<!-- submit button -->
-<p>{{ Form::submit('Login') }}</p>
-
-{{ Form::close() }}
 @stop
--- app/views/user/restore.blade.php
+++ app/views/user/restore.blade.php
@@ -1,13 +1,27 @@
 @extends('layout')
 
-@section('header')
-@stop
-
 @section('content')
 
-<form action="{{ action('UserController@restoreFormEndpoint') }}" method="POST">
-    <input type="email" name="email">
-    <input type="submit" value="Send Reminder">
-</form>
+<fieldset class="dialog-box-1" style="width:280px">
+    <legend>Remind password</legend>
+
+    <form action="{{ action('UserController@restoreFormEndpoint') }}" method="POST">
+        <table>
+            <tr>
+                <td>Your email:</td>
+                <td>&nbsp;</td>
+                <td><input class="my-input" type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td colspan="3">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <input class="my-btn" type="submit" value="Send to email">
+                </td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
 
 @stop
