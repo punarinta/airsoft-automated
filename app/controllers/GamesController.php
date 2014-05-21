15648857d7e272eeed4e8b649a945d985a0a9188
punarinta <cheaterx@yandex.ru>
2014-05-21 08:17:45 +0200

    bugfix

--- app/controllers/GamesController.php
+++ app/controllers/GamesController.php
@@ -32,15 +32,18 @@ class GamesController extends BaseController
             $gameData[$game->getId()]->ticket_templates = $ticket_templates;
             $gameData[$game->getId()]->bookable = !$ticket_templates->isEmpty();
 
-            // Check that ticket is not yet booked for you
-            $ticket = DB::table('ticket AS t')
-                ->select(array('t.id'))
-                ->join('ticket_template AS tt', 'tt.id', '=', 't.ticket_template_id')
-                ->where('tt.game_id', '=', $game->getId())
-                ->where('t.user_id', '=', Auth::user()->getId())
-                ->first();
+            if (Auth::check())
+            {
+                // Check that ticket is not yet booked for you
+                $ticket = DB::table('ticket AS t')
+                    ->select(array('t.id'))
+                    ->join('ticket_template AS tt', 'tt.id', '=', 't.ticket_template_id')
+                    ->where('tt.game_id', '=', $game->getId())
+                    ->where('t.user_id', '=', Auth::user()->getId())
+                    ->first();
 
-            $gameData[$game->getId()]->is_booked = !empty ($ticket);
+                $gameData[$game->getId()]->is_booked = !empty ($ticket);
+            }
         }
 
         return View::make('games', array('games' => $gameData));
