6bdce63bc06ebd319281cd8268ed21b90b8cd658
punarinta <cheaterx@yandex.ru>
2014-05-21 11:20:42 +0200

    favicon in layout

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -10,6 +10,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/js/jquery-2.1.1.min.js"><\/script>')</script>
     <script src="/js/az-core.js"></script>
+    <link rel="icon" href="/favicon.ico" />
 </head>
 <body>
 <div id="main">
