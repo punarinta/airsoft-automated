3ec3e3611a4715b494c410b221790e5c13e25825
punarinta <cheaterx@yandex.ru>
2014-06-07 13:17:21 +0200

    ru locale sketch

---
+++ app/lang/ru/airsoft.php
@@ -0,0 +1,17 @@
+<?php
+
+return array
+(
+    'menu' => array
+    (
+        'home' => 'Главная',
+        'games' => 'Игры',
+        'i_play' => 'Участвую',
+        'i_organize' => 'Организую',
+        'profile' => 'Профиль',
+        'about' => 'О нас',
+        'sign_in' => 'Войти',
+        'sign_out' => 'Выйти',
+        'no_nick' => 'без ника',
+    ),
+);
