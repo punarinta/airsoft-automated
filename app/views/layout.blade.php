f758a1bff18d511d67bfcb8f4bd25e11498a7ea5
punarinta <cheaterx@yandex.ru>
2014-06-07 13:56:40 +0200

    temporary disable switcher

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -43,18 +43,18 @@
         @endif
 
         <ul style="float:right">
-            <li><a class="first" href="#">
+<!--            <li><a class="first" href="#">
                     <select class="ddb-locale my-select">
                         <option value="en">EN</option>
                         <option value="se">SE</option>
                         <option value="ru">RU</option>
                     </select>
-            </a></li>
+            </a></li>-->
 
             @if(Auth::check())
-            <li><a class="last" href="{{ URL::route('logout') }}">{{ trans('airsoft.menu.sign_out') }} ({{ Auth::user()->nick ? Auth::user()->nick : trans('airsoft.menu.no_nick') }})</a></li>
+            <li><a class="alone" href="{{ URL::route('logout') }}">{{ trans('airsoft.menu.sign_out') }} ({{ Auth::user()->nick ? Auth::user()->nick : trans('airsoft.menu.no_nick') }})</a></li>
             @else
-            <li><a class="last" href="{{ URL::route('login') }}">{{ trans('airsoft.menu.sign_in') }}</a></li>
+            <li><a class="alone" href="{{ URL::route('login') }}">{{ trans('airsoft.menu.sign_in') }}</a></li>
             @endif
         </ul>
     </div>
