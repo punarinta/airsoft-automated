7f27c2bc90c341884083f85cc7de8c553591e2d3
punarinta <cheaterx@yandex.ru>
2014-06-16 16:00:03 +0200

    populate with users and teams

--- app/controllers/TestingController.php
+++ app/controllers/TestingController.php
@@ -17,6 +17,28 @@ class TestingController extends BaseController
                 throw new \Exception('Access denied.');
             }
 
+            // generate teams
+            for ($i = 1; $i <= 80; $i++)
+            {
+                $team = new Team;
+                $team->setName('Team ' . $i);
+                $team->setOwnerId(mt_rand(10, 500));
+                $team->setRegionId(mt_rand(1, 21));
+                $team->save();
+            }
+
+            // generate users
+            for ($i = 1; $i <= 500; $i++)
+            {
+                $user = new User;
+                $user->setNick('User ' . $i);
+                $user->setEmail('user-' . $i . '@jesp.ru');
+                $user->setTeamId(mt_rand(1, 80));
+                $user->setIsEmailValidated(1);
+                $user->setPassword('$2y$10$IiMiFJ5iuwGHXraFLPeBR..3QeUUVtPz6dP.Pndo5tLXZQpAZCnXW');
+                $user->save();
+            }
+
             return null;
         });
     }
