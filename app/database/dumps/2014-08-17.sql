49fc21348d025e6ad27bd5119d66f0fcb3e5985e
punarinta <cheaterx@yandex.ru>
2014-08-17 18:04:31 +0200

    dumps

---
+++ app/database/dumps/2014-08-17.sql
@@ -0,0 +1,376 @@
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(127) NOT NULL,
+  `code` char(3) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `country`
+--
+
+LOCK TABLES `country` WRITE;
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` VALUES (1,'Sweden','SWE',NULL,NULL);
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `owner_id` int(10) unsigned NOT NULL,
+  `region_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `starts_at` datetime DEFAULT NULL,
+  `ends_at` datetime DEFAULT NULL,
+  `is_visible` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `settings` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (1,'Test game 1',1,1,'2014-08-21 00:00:00','2014-08-22 00:00:00',1,NULL,NULL,NULL),(2,'Test game 2',1,4,'2014-08-28 00:00:00','2014-08-29 00:00:00',1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game_party`
+--
+
+DROP TABLE IF EXISTS `game_party`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_party` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(127) NOT NULL,
+  `game_id` int(10) unsigned NOT NULL,
+  `players_limit` int(10) unsigned NOT NULL,
+  `description` mediumtext,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_party`
+--
+
+LOCK TABLES `game_party` WRITE;
+/*!40000 ALTER TABLE `game_party` DISABLE KEYS */;
+INSERT INTO `game_party` VALUES (1,'Party 1',1,100,NULL,NULL,NULL),(2,'Party 2',1,200,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `game_party` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_reminder`
+--
+
+DROP TABLE IF EXISTS `password_reminder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_reminder` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `password_reminder_email_index` (`email`),
+  KEY `password_reminder_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_reminder`
+--
+
+LOCK TABLES `password_reminder` WRITE;
+/*!40000 ALTER TABLE `password_reminder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_reminder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `provider_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `transaction_id` varchar(64) DEFAULT NULL,
+  `amount` int(10) unsigned NOT NULL,
+  `status` int(10) unsigned NOT NULL DEFAULT '1',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,1,2,'tran_fa915923b21c8034b606bba68115',10000,16,'2014-06-08 08:53:01','2014-06-08 08:53:01');
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `region`
+--
+
+DROP TABLE IF EXISTS `region`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `region` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(127) NOT NULL,
+  `country_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `region`
+--
+
+LOCK TABLES `region` WRITE;
+/*!40000 ALTER TABLE `region` DISABLE KEYS */;
+INSERT INTO `region` VALUES (1,'Stockholm',1,NULL,NULL),(2,'Västerbotten',1,NULL,NULL),(3,'Norrbotten',1,NULL,NULL),(4,'Uppsala',1,NULL,NULL),(5,'Östergötland',1,NULL,NULL),(6,'Östergötland',1,NULL,NULL),(7,'Jönköping',1,NULL,NULL),(8,'Kronoberg',1,NULL,NULL),(9,'Kalmar',1,NULL,NULL),(10,'Gotland',1,NULL,NULL),(11,'Blekinge',1,NULL,NULL),(12,'Skåne',1,NULL,NULL),(13,'Halland',1,NULL,NULL),(14,'Västra Götaland',1,NULL,NULL),(15,'Värmland',1,NULL,NULL),(16,'Örebro',1,NULL,NULL),(17,'Västmanland',1,NULL,NULL),(18,'Dalarna',1,NULL,NULL),(19,'Gävleborg',1,NULL,NULL),(20,'Västernorrland',1,NULL,NULL),(21,'Jämtland',1,NULL,NULL);
+/*!40000 ALTER TABLE `region` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shop`
+--
+
+DROP TABLE IF EXISTS `shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shop` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `plugin` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shop`
+--
+
+LOCK TABLES `shop` WRITE;
+/*!40000 ALTER TABLE `shop` DISABLE KEYS */;
+INSERT INTO `shop` VALUES (1,'AirsoftSverige','airsoftsverige_com'),(2,'Röda Stjärnan','rodastjarnan_com'),(3,'Frysen Airsoft','frysen_nu'),(4,'Airsoftbutiken','airsoftbutiken_se'),(5,'Wizeguy','wizeguy_se'),(6,'Striker Airsoft','strikerairsoft_se');
+/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shop_vote`
+--
+
+DROP TABLE IF EXISTS `shop_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shop_vote` (
+  `shop_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `vote` int(11) DEFAULT '0',
+  KEY `shop_id` (`shop_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shop_vote`
+--
+
+LOCK TABLES `shop_vote` WRITE;
+/*!40000 ALTER TABLE `shop_vote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shop_vote` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `region_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `owner_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+INSERT INTO `team` VALUES (1,'Some team',1,2,NULL,NULL,NULL),(2,'Org\'s team',1,1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `game_party_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `ticket_template_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `host_ticket_id` int(10) unsigned DEFAULT NULL,
+  `payment_id` int(10) unsigned DEFAULT NULL,
+  `status` int(10) unsigned NOT NULL DEFAULT '1',
+  `netto` int(10) unsigned NOT NULL DEFAULT '0',
+  `brutto` int(10) unsigned NOT NULL DEFAULT '0',
+  `vat` int(10) unsigned NOT NULL DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket_template`
+--
+
+DROP TABLE IF EXISTS `ticket_template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket_template` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `game_id` int(10) unsigned NOT NULL,
+  `game_party_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `price` int(10) unsigned NOT NULL,
+  `price_date_start` datetime DEFAULT NULL,
+  `price_date_end` datetime DEFAULT NULL,
+  `is_cash` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `notes` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket_template`
+--
+
+LOCK TABLES `ticket_template` WRITE;
+/*!40000 ALTER TABLE `ticket_template` DISABLE KEYS */;
+INSERT INTO `ticket_template` VALUES (1,1,0,15000,'2014-06-01 00:00:00','2014-12-08 00:00:00',0,NULL,NULL,NULL),(2,1,1,10000,'2014-06-01 00:00:00','2014-11-30 00:00:00',0,NULL,NULL,NULL),(3,1,2,11000,'2014-06-01 00:00:00','2014-12-08 00:00:00',1,NULL,NULL,NULL),(4,1,1,12300,'2014-12-01 00:00:00','2014-12-08 00:00:00',0,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `ticket_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL,
+  `nick` varchar(63) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `team_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `is_team_manager` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `is_validated` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `is_email_validated` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `settings` text,
+  `profile` text,
+  `remember_token` char(60) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'org@jesp.ru','$2y$10$IiMiFJ5iuwGHXraFLPeBR..3QeUUVtPz6dP.Pndo5tLXZQpAZCnXW','Organizer','2014-05-01',2,0,1,1,'{\"locale\":\"en\"}','[]','DQJamVeshTfLpwqWXCkhzwCYicwbfxuTOTuhtIswSlYbrpjcw0Ih9YiQhIo0','2014-05-06 00:00:00','2014-06-13 12:39:29'),(2,'player-1@jesp.ru','$2y$10$IiMiFJ5iuwGHXraFLPeBR..3QeUUVtPz6dP.Pndo5tLXZQpAZCnXW','Player 1',NULL,1,1,0,1,NULL,NULL,'5fY7bA7RWoiUC4snYRONq9Bjjes5An4lIzrOC1gqlrdRKNuutWuLx2tq7rgr','2014-05-06 00:00:00','2014-05-19 13:06:57'),(3,'player-2@jesp.ru','$2y$10$IiMiFJ5iuwGHXraFLPeBR..3QeUUVtPz6dP.Pndo5tLXZQpAZCnXW','Player 2',NULL,1,0,0,0,NULL,NULL,'O6WNpkvw7Hxji4GTbFcGyO3jyPDATRnk2RP8FK5Zj7d4XxVH7BTt9R6O9rMs','2014-05-06 00:00:00','2014-06-07 11:05:16'),(4,'player-3@jesp.ru','$2y$10$IiMiFJ5iuwGHXraFLPeBR..3QeUUVtPz6dP.Pndo5tLXZQpAZCnXW','Player 3',NULL,1,0,0,1,NULL,NULL,NULL,'2014-05-06 00:00:00','2014-05-10 15:35:39');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-17 18:03:13
