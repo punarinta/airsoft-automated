e07247e7653f68edc812802f5ef57d1ae64cd70b
punarinta <cheaterx@yandex.ru>
2014-08-18 10:38:15 +0200

    strikerairsoft fix

--- public/sys/market/scan/strikerairsoft_se.php
+++ public/sys/market/scan/strikerairsoft_se.php
@@ -16,6 +16,54 @@ class Strikerairsoft_Se extends ScanMarket
             return 2;
         }
 
+        // try 'big' items first
+
+        $this->grab($html, 'shopwindow_grid', null);
+
+        while (1)
+        {
+            if (!$firstgrab = $this->grab($html, 'gridItemContainer', null))
+            {
+                break;
+            }
+
+            $item = new ScanMarketItem($this->itemsType);
+
+            $item->id = $this->grab($html, ' article-', '">');
+            $item->url = 'http://www.strikerairsoft.se' . $this->grab($html, '<a href="', '">');
+
+            $img = $this->grab($html, '<img src="', '" ');
+            $img = explode('.', $img);
+            if (sizeof($img) > 3)
+            {
+                $ext = explode('_', $img[4]);
+                $ext = $ext[0];
+            }
+            else
+            {
+                $ext = '';
+            }
+
+            $item->img = $img[0] . '.' . $img[1] . '.' . $img[2] . '.' . $ext;
+            $item->name = trim(strip_tags($this->grab($html, 'gridArticleName">', '</a>')));
+            $item->descr = trim(strip_tags($this->grab($html, 'gridArticleText">', '</div>')));
+            $item->price = (int) preg_replace('/[^0-9,]/', '', $this->grab($html, 'gridArticlePrice">', ':'));
+
+            $data = $this->grab($html, 'gridArticleButtons', '</td>');
+
+            if (strpos($data, '"Bevaka"') !== false)
+            {
+                $item->stock = 0;
+            }
+
+            if ($this->pushRow($item) == -1)
+            {
+                break;
+            }
+        }
+
+        // try everything else
+
         $this->grab($html, 'articleListTable', null);
 
         while (1)
@@ -45,7 +93,7 @@ class Strikerairsoft_Se extends ScanMarket
             $item->img = $img[0] . '.' . $img[1] . '.' . $img[2] . '.' . $ext;
             $item->name = trim(strip_tags($this->grab($html, 'listArticleName">', '</a>')));
             $item->descr = trim(strip_tags($this->grab($html, 'listArticleTextContainer">', '</div>')));
-            $item->price = (int) preg_replace('/[^0-9,]/', '', $this->grab($html, 'listArticlePrice">', '</span>'));
+            $item->price = (int) preg_replace('/[^0-9,]/', '', $this->grab($html, 'listArticlePrice">', ':'));
 
             $data = $this->grab($html, 'listLineBuyButtonColumn', '</td>');
 
