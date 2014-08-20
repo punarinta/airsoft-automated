be21fe10a8bf680ed53b2cac8485834fb409b70d
punarinta <cheaterx@yandex.ru>
2014-08-20 10:11:58 +0200

    GA update

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -64,9 +64,10 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga')
-ga('create', 'UA-51418351-1', 'airsoft.zone')
-ga('send', 'pageview')
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-51418351-1', 'airsoft.zone');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
 
 $('#navbar .ddb-locale').val('{{ App::getLocale() }}').change(function(){$.ajax({url:'/api/session',type:'PUT',dataType:'json',data:JSON.stringify({locale:$('#navbar .ddb-locale').val()}),success:function(){location.reload()}})})
 </script>
